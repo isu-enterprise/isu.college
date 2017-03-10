09dd1d2dc86f0ab358f7b3b64610fd81e1ff8b25
Evgeny Cherkashin <eugene@irnok.net>
2017-03-10 18:52:24 +0800

    Lost (not all) paragraphs added.

--- data/rdfa2ttl.sh
+++ data/rdfa2ttl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python2 pyrdfa3/scripts/localRDFa.py -b https://irnok.net/pages/standard-09.03.01-bachelor-2016.html ./standard-09.03.01-bachelor-2016.html > standard-09.03.01-bachelor-2016.ttl
+python2 pyrdfa3/scripts/localRDFa.py -s -b https://irnok.net/pages/standard-09.03.01-bachelor-2016.html ./standard-09.03.01-bachelor-2016.html > standard-09.03.01-bachelor-2016.ttl
