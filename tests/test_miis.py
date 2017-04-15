de85a2786924bf4f14f99dbfb29d5631969b45cc
Evgeny Cherkashin <eugene@irnok.net>
2017-04-15 15:43:59 +0800

    Collect all miis's plans in the data dir.

--- tests/test_miis.py
+++ tests/test_miis.py
@@ -2,13 +2,40 @@ from nose.tools import raises
 from isu.college.miis import Plan
 from pkg_resources import resource_filename
 import os.path
+import glob
+from pprint import pprint
 
 DATADIR = os.path.abspath(
     os.path.join(
-        resource_filename('isu.college', "data/study-plans/plans"), "../../../")
+        resource_filename(
+            'isu.college',
+            "../../../"),
+        "data/study-plans/plans/")
 )
+
+# TMPL = "*.pl*.xml.xls"
+TMPL = "*.xls"
+
+template = DATADIR + "/" + TMPL
+
+# print(template)
+
+ALLIN = glob.glob(template)
+
+# print(DATADIR)
+# print(len(ALLIN))
+# pprint(ALLIN)
+
+
+class TestBasic(object):
+
+    def test_datadir(self):
+        assert os.path.isdir(DATADIR)
+
+    def test_allin_not_empty(self):
+        assert len(ALLIN) > 0
 
 
 class TestLoad(object):
     def setUp(self):
-        self.plan = ()
+        self.plan = Plan(ALLIN[0])
